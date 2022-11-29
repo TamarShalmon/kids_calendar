b52350ff186a9aeb9895ed5fb70c6712285f3d76
TamarShalmon <105917152+TamarShalmon@users.noreply.github.com>
2022-11-29 14:03:26 +0200

    Update src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
 import './App.css';
+import Week from './Week';
+import days from '../days';
+import Events from './Events';
+import events from '../events';
+
+const Title = styled.h2`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
 
 function App() {
+
   return (
     <div className="container">
-  
+      <Title>Hello user, L'ets plan your week!</Title>
+      <Week days={days} />
+      <div>
+        <h3>Events board</h3>
+      <Events events={events} />
+      </div>
     </div>
   );
 }

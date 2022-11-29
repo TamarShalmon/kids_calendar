88abe14ebac2426adbfc1eca62400fa2e8348c3f
TamarShalmon <105917152+TamarShalmon@users.noreply.github.com>
2022-11-29 14:03:14 +0200

    Create src/events.js

--- src/events.js
+++ src/events.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export default [
     {
         id: 0,
@@ -112,3 +113,57 @@ export default [
 ];
 
 
+=======
+export default [
+    {
+        id: 1,
+        title: "shower",
+        category: "",
+        image: "",
+    },
+    {
+        id: 2,
+        title: "brush teeth",
+        category: "",
+        image: "",
+    }, 
+    {
+        id: 3,
+        title: "get dress",
+        category: "",
+        image: "",
+    }, 
+    {
+        id: 4,
+        title: "kindergarten",
+        category: "",
+        image: "",
+    },
+    {
+        id: 5,
+        title: "kindergarten",
+        category: "",
+        image: "",
+    },
+    {
+        id: 6,
+        title: "shower",
+        category: "",
+        image: "",
+    },
+    {
+        id: 7,
+        title: "shower",
+        category: "",
+        image: "",
+    },
+    {
+        id: 8,
+        title: "shower",
+        category: "",
+        image: "",
+    },
+];
+
+
+>>>>>>> 76512f6 (Create src/events.js)
